e22d022f99864da3131ed550b8d1230c0ca67c8e
tejaspawar2026 <126851519+tejaspawar2026@users.noreply.github.com>
2023-08-15 17:55:18 +0530

    v1.1.9.4
    tejas

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,22 +1,5 @@
 @extends('layouts.guest.guest')
 @section('guest')
 Welcome
-@php
-    $cast=App\Models\Cast::all();
-    $category=App\Models\Category::all();
-
-@endphp
-<table class="table bg-dark text-danger" style="color:black;">
-    <tr>
-        <th>Cast</th>
-        <th>Category</th>
-    </tr>
-    @foreach ($cast as $c)
-    <tr>
-        <td>{{ $c->name }}</td>
-        <td>{{ $c->category->name }}</td>
-    </tr>@break
-    @endforeach
-</table>
 @endsection
 
